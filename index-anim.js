f2aff2ac561639ffc411b739271dade40da534df
Joefrey Mahusay <joefreymahusay@Joefreys-MacBook-Pro.local>
2023-12-08 06:11:42 +0800

    Initial commit

---
+++ index-anim.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  resetElements();
+
+  gsap.utils.toArray(".gsap-section").forEach(function (elem) {
+    ScrollTrigger.create({
+      trigger: elem,
+      start: "top 80%",
+      end: "bottom 20%",
+      markers: false,
+      once: true,
+      onEnter: function () {
+        let sectionID = elem.id;
+
+        if (sectionID === "hero") {
+          // heroAnimate();
+          heroCurveAnimate();
+        }
+        // else if (sectionID === "stats") {
+        //   statsAnimate();
+        //   bubblesAnimate();
+        // }
+      },
+    });
+  });
+  function heroAnimate() {}
+  function heroCurveAnimate() {
+    const path = document.querySelectorAll("#curve-hero path");
+
+    const svgLinesTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: path,
+        start: "top 90%",
+        end: "+=270%",
+        scrub: false,
+        once: false,
+        pin: false,
+        markers: false,
+      },
+    });
+
+    path.forEach((el) => {
+      svgLinesTL.to(
+        el,
+        1000,
+        {
+          strokeDashoffset: 0,
+          delay: 2,
+          strokeDasharray: el.getTotalLength(),
+          ease: "power4.inOut",
+        },
+        "start"
+      );
+    });
+  }
+});
