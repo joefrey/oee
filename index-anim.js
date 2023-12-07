af5b8abcf51bebf201ca429632077e9225841bb1
Joefrey Mahusay <joefreymahusay@Joefreys-MacBook-Pro.local>
2023-12-08 07:34:30 +0800

    Initial commit

--- index-anim.js
+++ index-anim.js
@@ -28,21 +28,20 @@ $(document).ready(function () {
   function heroCurveAnimate() {
     const path = document.querySelectorAll("#curve-hero path");
     console.log("enter animte");
-    // const svgLinesTL = gsap.timeline();
-    const svgLinesTL = gsap.timeline({
-      scrollTrigger: {
-        trigger: path,
-        start: "top 90%",
-        end: "+=270%",
-        scrub: true,
-        once: false,
-        pin: false,
-        markers: false,
-      },
-    });
+    const svgLinesTL = gsap.timeline();
+    // const svgLinesTL = gsap.timeline({
+    //   scrollTrigger: {
+    //     trigger: path,
+    //     start: "top 90%",
+    //     end: "+=270%",
+    //     scrub: true,
+    //     once: false,
+    //     pin: false,
+    //     markers: false,
+    //   },
+    // });
 
     path.forEach((el) => {
-      console.log(el.getTotalLength());
       svgLinesTL.to(
         el,
         1000,
