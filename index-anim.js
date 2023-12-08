d36cebbf9612970214ec479c812b4cf6307292c6
Joefrey Mahusay <joefreymahusay@Joefreys-MacBook-Pro.local>
2023-12-08 13:51:34 +0800

    Initial commit

--- index-anim.js
+++ index-anim.js
@@ -125,7 +125,7 @@ $(document).ready(function () {
         el,
         3,
         {
-          strokeDashoffset: 0,
+          strokeDashoffset: el.getTotalLength() * 2,
           strokeDasharray: el.getTotalLength(),
           ease: "expo.inOut",
         },
