31e706c3ee566078d06e92fb0078bdc8748d49fd
Joefrey Mahusay <joefreymahusay@Joefreys-MacBook-Pro.local>
2023-12-08 09:47:03 +0800

    Initial commit

--- index-anim.js
+++ index-anim.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
           strokeDasharray: el.getTotalLength(),
           ease: "expo.inOut",
         },
-        "-=.5"
+        "start"
       );
     });
     const image = document.querySelector("#image0_301_15");
